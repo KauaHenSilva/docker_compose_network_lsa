2a133e3803457526fd3fb1c631a656c21d65447a
KauaHenSilva <kaua.sbc@gmail.com>
2025-04-15 21:47:11 -0300

    feat: add msg error

--- router/router.py
+++ router/router.py
@@ -7,8 +7,11 @@ time.sleep(5)
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
 
-print(f"Hostname: {hostname} / IP Address: {ip_address}")
+print(f"Hostname: {hostname} / IP Address: {ip_address}", flush=True)
 
 router_links = os.environ.get('router_links')
 for link in router_links.split(','):
-  print(f"Router link: {link} -> {socket.gethostbyname(link)}")
+  try:
+    print(f"Router link: {link} -> {socket.gethostbyname(link)}", flush=True)
+  except socket.gaierror:
+    print(f"Router link: {link} -> Not found", flush=True)
